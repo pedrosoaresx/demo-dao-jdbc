ef414697023b1b0d6982cab304560972b57900fc
Pedro Soares <asp.pedro30@gmail.com>
2026-03-02 20:47:09 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
         Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0, dep);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n===Test 5: Seller Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+        System.out.println("Updated complete");
     }
 }
